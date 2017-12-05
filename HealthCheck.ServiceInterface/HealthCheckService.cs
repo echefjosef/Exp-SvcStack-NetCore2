2e6610fc0ce35ea2a9d212b0d9cabf8e0ae1bb9c
Kurt Schoener <kurt.schoener@progleasing.com>
2017-12-05 01:41:06 -0700

    Nonfunctioning ServiceStack service with simple healthcheck endpoint
    > Add ServiceStack 1.0.44 to Startup project
    > Add HealthCheck.ServiceModel and .ServiceInterface projects
    > Add ServiceStack 1.0.44 to HealthCheck.ServiceInterface
    > Leave dependency on Microsoft.AspNetCore.All alone
    > Solution builds and runs find locally
    > Publish produces 36 files and runtimes directory
    > Webapp does not run properly. 502.5 error.
    > The published content is nearly half that of the service that is
      only dependent on Microsoft.AspNetCore. I do not understand why.

---
+++ HealthCheck.ServiceInterface/HealthCheckService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HealthCheck.ServiceModel;
+using ServiceStack;
+
+namespace HealthCheck.ServiceInterface
+{
+    public class HealthCheckService : Service
+    {
+        public object Get(HealthCheckRequest request)
+        {
+            return "Service is up and running...";
+        }
+    }
+}
---
+++ Startup/AppHost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Funq;
+using HealthCheck.ServiceInterface;
+using HealthCheck.ServiceModel;
+using ServiceStack;
+
+namespace Startup
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : 
+            base("Exp ServiceStack & .Net Core 2", 
+                typeof(HealthCheckService).Assembly)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            ConfigureRoutes();
+        }
+
+        private void ConfigureRoutes()
+        {
+            Routes.Add<HealthCheckRequest>("/healthcheck", ApplyTo.Get);
+        }
+    }
+}
